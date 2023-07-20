146bf39b3d7d4f632eb3809f824f56c0aa718582
KristyAcquah <akosuaaqcuah99@gmail.com>
2023-07-20 15:56:59 -0700

    corrections

--- VC_path.c
+++ VC_path.c
@@ -7,14 +7,8 @@
 
 #define MAX_INSTRUCTION_LENGTH 200
 
-/**
- * showShellPrompt - display prompt "(teamwork$) "
- */
-
-void showShellPrompt(void)
-{
-	printf("(teamwork$) ");
-}
+/* prototype */
+void showShellPrompt(void);
 
 
 /**
@@ -50,56 +44,4 @@ void runShellinstruction(const char *instruction, char *args[])
 			fprintf(stderr, "An error occurred: %s\n", instruction);
 		}
 	}
-}
-
-/**
- * main - the function for the shell
- * @argc: the number of command-line arguments
- * @argv: an array of string containing command-line arguments
- *
- * Return: Always 0
- */
-
-int main(int argc, char *argv[])
-{
-	char instruction[MAX_INSTRUCTION_LENGTH];
-	char *command;
-	char *arg;
-	char *args[MAX_INSTRUCTION_LENGTH];
-
-	if (argc > 1)
-	{
-		for (int k = 1; k < argc; k++)
-		{
-			args[0] = "/bin/sh";
-			args[1] = "-c";
-			args[2] = argv[k];
-			args[3] = NULL;
-			runShellinstruction(argv[k], args);
-		}
-	}
-
-	while (1) /* using while loop to iterate the beginning procss */
-	{
-		showShellPrompt();
-
-		if (fgets(instruction, MAX_INSTRUCTION_LENGTH, stdin) == NULL)
-		{
-			break;
-		}
-
-		instruction[strcspn(instruction, "\n")] = '\0'; /* Clear trailing newlines */
-		if (strcmp(instruction, "exit") == 0)
-		{
-			break;
-		}
-		command = strtok(instruction, " ");
-		arg = strtok(NULL, " ");
-
-		args[0] = command;
-		args[1] = arg;
-		args[2] = NULL;
-		runShellinstruction(command, args);
-	}
-	return (0);
 }
--- VC_shell.c
+++ VC_shell.c
@@ -7,51 +7,40 @@
 
 #define MAX_INSTRUCTION_LENGTH 200
 
-/**
- * showShellPrompt - Displays shell prompt
- */
-void showShellPrompt(void)
+/* Function Prototype */
+void showShellPrompt(void);
+void runShellinstruction(const char *instruction);
+
 {
-printf("(teamwork$) ");
-}
+	pid_t pid = fork(); /* creating a new child process using fork */
 
-/**
- * runShellInstruction - Execute the given shell instruction
- * @instruction: instruction to be executed
- */
-void runShellInstruction(const char *instruction)
-{
-pid_t pid = fork(); /* creating a new child process using fork */
+	if (pid == 0) /* Result when it is in the child process */
+	{
+		/* Child process using execve to execute the instruction from user */
+		char *const args[] = {"/bin/sh", "-c", NULL};
 
-if (pid == 0) /* Result when it is in the child process */
-{
-/* Child process using execve to execute the instruction from user */
-char *const args[] = {"/bin/sh", "-c", NULL};
+		execve("/bin/sh", args, NULL);
+		perror("execve"); /* execve() only returns if an error occurs */
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == -1) /* Result if there was an error during compilation */
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0) /* Result if it is in the parent process */
+	{
+		/* The Parent process waits for the child process to terminate */
+		int status;
 
+		waitpid(pid, &status, 0);
 
-execve("/bin/sh", args, NULL);
-
-perror("execve"); /* execve() only returns if an error occurs */
-exit(EXIT_FAILURE);
-}
-else if (pid == -1) /* Result if there was an error during compilation */
-{
-perror("fork");
-exit(EXIT_FAILURE);
-}
-else if (pid > 0) /* Result if it is in the parent process */
-{
-/* The Parent process waits for the child process to terminate */
-int status;
-
-waitpid(pid, &status, 0);
-
-if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-{
-/* Checking if the child process exited normally or not */
-fprintf(stderr, "Error occurred while running code: %s\n", instruction);
-}
-}
+		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+		{
+			/* Checking if the child process exited normally or not */
+			fprintf(stderr, "Error occurred while running code: %s\n", instruction);
+		}
+	}
 }
 
 /**
@@ -62,38 +51,37 @@ fprintf(stderr, "Error occurred while running code: %s\n", instruction);
  */
 int main(int argc, char *argv[])
 {
-if (argc > 1)
-{
-/* If there are arguments provided, enters Non-interactive mode */
-int k;
-for (k = 1; k < argc; k++)
-{
-runShellInstruction(argv[k]);
-}
-}
-else
-{
-while (1) /* using while loop to iterate the beginning process */
-{
-char instruction[MAX_INSTRUCTION_LENGTH];
-showShellPrompt(); /* Displays the prompt */
+	if (argc > 1)
+	{
+		/* If there are arguments provided, enters Non-interactive mode */
+		int k;
+		
+		for (k = 1; k < argc; k++)
+		{
+			runShellinstruction(argv[k]);
+		}
+	}	
+	else
+	{
+		while (1) /* using while loop to iterate the beginning process */
+		{
+			char instruction[MAX_INSTRUCTION_LENGTH];
+			showShellPrompt(); /* Displays the prompt */
 
-if (fgets(instruction, MAX_INSTRUCTION_LENGTH, stdin) == NULL)
-{
-break; /* Exit if Ctrl+D is pressed */
-}
+			if (fgets(instruction, MAX_INSTRUCTION_LENGTH, stdin) == NULL)
+			{
+				break; /* Exit if Ctrl+D is pressed */
+			}
 
-instruction[strcspn(instruction, "\n")] = '\0'; /* Clear newlines */
+			instruction[strcspn(instruction, "\n")] = '\0'; /* Clear newlines */
 
-if (strcmp(instruction, "exit") == 0)
-{
-break; /* Exit shell if the instruction is "exit" */
-}
+			if (strcmp(instruction, "exit") == 0)
+			{
+				break; /* Exit shell if the instruction is "exit" */
+			}
+			runShellinstruction(instruction); /* Execute instruction */
+		}
+	}
 
-runShellInstruction(instruction); /* Execute instruction */
+	return (0);
 }
-}
-
-return (0);
-}
-
